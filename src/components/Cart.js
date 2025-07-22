753fb3d93f608c031a80b5cab0de98b7e1ad8141
Garik Davtyan <ggd260907@gmail.com>
2025-07-22 15:20:47 +0400

    init

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div style={{padding: 32, maxWidth: 600, margin: '0 auto'}}>
+      <h1>Корзина</h1>
+      <p>Здесь могла бы быть ваша корзина.</p>
+    </div>
+  );
+}
+
+export default Cart;
